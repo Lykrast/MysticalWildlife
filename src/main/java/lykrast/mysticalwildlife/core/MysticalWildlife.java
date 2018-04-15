b4aafeac47af2bb46079369d1405ba136bbce14b
Lykrast <aymericpm@gmail.com>
2018-04-15 10:13:06 +0200

    Readying version number for upcoming release

--- src/main/java/lykrast/mysticalwildlife/core/MysticalWildlife.java
+++ src/main/java/lykrast/mysticalwildlife/core/MysticalWildlife.java
@@ -15,7 +15,7 @@ public class MysticalWildlife
 {
     public static final String MODID = "mysticalwildlife";
     public static final String NAME = "Mystical Wildlife";
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "1.0.0";
 
 	@Instance
 	public static MysticalWildlife instance;
