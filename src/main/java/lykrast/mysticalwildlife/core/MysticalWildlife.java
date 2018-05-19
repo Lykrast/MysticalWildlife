e646157b958b51ea077547c4133c0cfd97dbcb5f
Lykrast <aymericpm@gmail.com>
2018-05-19 14:34:43 +0200

    Bumped version to 1.2.0 and updated building tasks

--- src/main/java/lykrast/mysticalwildlife/core/MysticalWildlife.java
+++ src/main/java/lykrast/mysticalwildlife/core/MysticalWildlife.java
@@ -15,7 +15,7 @@ public class MysticalWildlife
 {
     public static final String MODID = "mysticalwildlife";
     public static final String NAME = "Mystical Wildlife";
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "@VERSION@";
 
 	@Instance
 	public static MysticalWildlife instance;
